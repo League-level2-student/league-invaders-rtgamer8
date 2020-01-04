fe1ca1c9f019131b44f0f3cf96a441f863b18052
League Student <league@iMac-37.attlocal.net>
2020-01-04 14:29:37 -0800

    Finished Snake, srated League Invaders :thumbsup: +1:

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,36 @@
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.Graphics;
 public class LeagueInvaders {
+	static JFrame frame;
+	
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	
+public static void main(String[] args) {
+	
+	GamePanel panel = new GamePanel();
+
+
+
+frame = new JFrame();
+frame.setVisible(true);
+frame.add(panel);
+frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+frame.pack();
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 
 }
+
+
+
+	
+} 
+
+
+
+
